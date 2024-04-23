4d70bd95bfeee5c19bee7bbc4e0190ad6897d145
Kevinw1304 <167887511+Kevinw1304@users.noreply.github.com>
2024-04-23 17:00:56 +0000

    reto_4_kevinw1304

---
+++ retos/reto_04/soluciones/kevinw1304.py
@@ -0,0 +1,25 @@
+
+def incremento (n):
+    return n+1
+        
+def decremento (n):        
+    return n-1
+
+def suma (n,m):
+    for i in range (m):
+        n=incremento (n)
+    return n
+
+def resta (n,m):
+    for i in range (m):
+        n=decremento (n)
+    return n    
+
+def multiplicación (n,m):
+     acumulador = 0
+     for i in range (m):
+        acumulador=suma(acumulador,n)
+    return acumulador    
+
+
+
